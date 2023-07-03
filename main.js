4ebdce84b6a176d4b27029e84f31c8f3483f71ef
Daniel Pretki <thepretki@gmail.com>
2023-07-03 20:41:58 +0200

    Add Bookmarks/ Groups of bookmarks Done
    -added ability to add new groups to view
    - function for cancel btn in form
    - fixed bug new form doesn't not reset on submit/close or page refres
    -!!! what is left to add validation to not add empty bookmarks or groups

--- main.js
+++ main.js
@@ -3,97 +3,42 @@ let retString = localStorage.getItem("Bookmarks");
 let bookmarks = JSON.parse(retString);
 console.log(bookmarks);
 
-// Bookmarks JSON Tameplate
-// [
-//     {
-//       groups: [
-//         {
-//           groupName: "nameOfBookmarkGroup1",
-//           bookmark: [
-//             {
-//               name: "nameOfBookmark 1-1",
-//               url: "urlOfBookmark 1-1"
-//             },
-//             {
-//               name: "nameOfBookmark 1-2",
-//               url: "urlOfBookmark 1-2"
-//             },
-//             {
-//               name: "nameOfBookmark 1-3",
-//               url: "urlOfBookmark 1-3"
-//             },
-//             {
-//               name: "nameOfBookmark 1-4",
-//               url: "urlOfBookmark 1-4"
-//             },
-//             {
-//               name: "nameOfBookmark 1-4",
-//               url: "urlOfBookmark 1-4"
-//             },
-//             {
-//               name: "nameOfBookmark 1-5",
-//               url: "urlOfBookmark 1-5"
-//             }
-//           ]
-//         },
-//         {
-//           groupName: "nameOfBookmarkGroup 2",
-//           bookmark: [
-//             {
-//               name: "nameOfBookmark 2-1",
-//               url: "urlOfBookmark 2-1"
-//             },
-//             {
-//               name: "nameOfBookmark 2-2",
-//               url: "urlOfBookmark 2-2"
-//             },
-//             {
-//               name: "nameOfBookmark 2-3",
-//               url: "urlOfBookmark 2-3"
-//             },
-//             {
-//               name: "nameOfBookmark 2-3",
-//               url: "urlOfBookmark 2-3"
-//             },
-//             {
-//               name: "nameOfBookmark 2-5",
-//               url: "urlOfBookmark 2-4"
-//             },
-//             {
-//               name: "nameOfBookmark 2-6",
-//               url: "urlOfBookmark 2-6"
-//             },
-//             {
-//               name: "nameOfBookmark 2-7",
-//               url: "urlOfBookmark 2-7"
-//             },
-//             {
-//               name: "nameOfBookmark 2-8",
-//               url: "urlOfBookmark 2-8"
-//             }
-//           ]
-//         }
-//       ]
-//     }
-//   ]
-
 // SELECT EXISTING ELEMENTS
 
 const body = document.querySelector("body");
 const bookmarksSection = document.querySelector(".main-section-bookmarks");
 
-const bookmarksGroups = bookmarks[0].groups;
+// remove child from section
 
-// addBookmark Logic
+function removeAllChildNodes(e) {
+  while (e.firstChild) {
+    e.removeChild(e.firstChild);
+  }
+}
+
+//
+/// addBookmark Container logic
+//
+
 const addBookmark = document.querySelector(".addBookmark");
 const addShowBookmark = document.querySelector(".addBookmark-btn");
-const addBookmarkInpName = document.querySelector(
-  ".addBookmark-container-name"
+const addBookmarkInpName = document.querySelector(".addBookmark-form-name");
+const addBookmarkInpUrl = document.querySelector(".addBookmark-form-url");
+const addBookmarkBtnAdd = document.querySelector(".addBookmark-form-btn-add");
+const addBookmarksBtnCancel = document.querySelector(
+  ".addBookmark-form-btn-cancel"
 );
-const addBookmarkInpUrl = document.querySelector(".addBookmark-container-url");
-const addBookmarkBtn = document.querySelector(".addBookmark-container-btn");
-const addBookmarkSelectGroup = document.querySelector(".addBookmark-container-groups");
-console.log(addBookmarkSelectGroup)
+const addBookmarkSelectGroup = document.querySelector(
+  ".addBookmark-form-chooseGroup"
+);
+const newGroupCheckBox = document.querySelector(".addBookmark-form-checkbox");
+const addBookmarksNewGroup = document.querySelector(
+  ".addBookmark-form-newGroup"
+);
+const checkToAddNewGroup = document.querySelectorAll(".group");
+
+// show/hide addBookmark card
+const bookmarksGroups = bookmarks[0].groups;
 
 let isAddBookmarkVisible = "false";
 addShowBookmark.addEventListener("click", function openWindow() {
@@ -105,35 +50,23 @@ addShowBookmark.addEventListener("click", function openWindow() {
   }
 });
 
-addBookmarkBtn.addEventListener("click", function getBalue() {
-  const name = addBookmarkInpName.value;
-  const url = addBookmarkInpUrl.value;
-  const newBookmark = {
-    Name: name,
-    Url: url,
-  };
-  bookmarks.push(newBookmark);
-  let bookmarksString = JSON.stringify(bookmarks);
-  localStorage.setItem("Bookmarks", bookmarksString);
-});
-
-bookmarksGroups.forEach((element) => {
-  const option = document.createElement("option");
-  option.textContent = element.groupName;
-  option.setAttribute("value", element.groupName);
-  addBookmarkSelectGroup.appendChild(option);
-
+// close addBookmarks card
+addBookmarksBtnCancel.addEventListener("click", function close() {
+  isAddBookmarkVisible = true;
+  addBookmark.setAttribute("data-visible", "false");
+  checkToAddNewGroup[0].setAttribute("data-visible", "true");
+  checkToAddNewGroup[1].setAttribute("data-visible", "false");
+  document.querySelector(".addBookmark-form").reset();
 });
 
 //add bookmarks to DOM
 const appendBookmarks = () => {
+  removeAllChildNodes(bookmarksSection);
   bookmarksGroups.forEach((element) => {
     const ul = document.createElement("ul");
     ul.textContent = element.groupName;
     bookmarksSection.appendChild(ul);
     const groupName = element.bookmark;
-    console.log(element.bookmark);
-
     groupName.forEach((element) => {
       const li = document.createElement("li");
       const a = document.createElement("a");
@@ -145,3 +78,85 @@ const appendBookmarks = () => {
   });
 };
 appendBookmarks();
+
+//add new bookmark to group or create new group
+addBookmarkBtnAdd.addEventListener("click", function getBalue() {
+  const name = addBookmarkInpName.value;
+  const url = addBookmarkInpUrl.value;
+  const group = addBookmarkSelectGroup.value;
+  const newGroup = addBookmarksNewGroup.value;
+
+  if (newGroupCheckBox.checked === false) {
+    const position = getPosition(group, bookmarksGroups);
+    const newBookmark = {
+      name: name,
+      url: url,
+    };
+    const arrBookmark = bookmarks[0].groups[position].bookmark;
+    arrBookmark.push(newBookmark);
+    let bookmarksString = JSON.stringify(bookmarks);
+    localStorage.setItem("Bookmarks", bookmarksString);
+  } else {
+    const newBookmark = {
+      groupName: newGroup,
+      bookmark: [
+        {
+          name: name,
+          url: url,
+        },
+      ],
+    };
+
+    const arrBookmark = bookmarks[0].groups;
+    arrBookmark.push(newBookmark);
+    let bookmarksString = JSON.stringify(bookmarks);
+    localStorage.setItem("Bookmarks", bookmarksString);
+  }
+  // newGroupCheckBox.checked = false;
+  removeAllChildNodes(addBookmarkSelectGroup);
+  appendBookmarks();
+  groupSelect();
+  isAddBookmarkVisible = true;
+  addBookmark.setAttribute("data-visible", "false");
+  checkToAddNewGroup[0].setAttribute("data-visible", "true");
+  checkToAddNewGroup[1].setAttribute("data-visible", "false");
+  document.querySelector(".addBookmark-form").reset();
+});
+
+// append new group input
+newGroupCheckBox.addEventListener("change", () => {
+  checkToAddNewGroup.forEach((element) => {
+    if (newGroupCheckBox.checked === true) {
+      if (element.attributes[2].value === "true") {
+        element.setAttribute("data-visible", "false");
+      } else {
+        element.setAttribute("data-visible", "true");
+      }
+    } else {
+      if (element.attributes[2].value === "false") {
+        element.setAttribute("data-visible", "true");
+      } else {
+        element.setAttribute("data-visible", "false");
+      }
+    }
+  });
+});
+// append groups to select
+const groupSelect = () => {
+  bookmarksGroups.forEach((element) => {
+    const option = document.createElement("option");
+    option.textContent = element.groupName;
+    option.setAttribute("value", element.groupName);
+    addBookmarkSelectGroup.appendChild(option);
+  });
+};
+groupSelect();
+
+// find possition in array
+function getPosition(elementToFind, arrayElements) {
+  return arrayElements
+    .map(function (e) {
+      return e.groupName;
+    })
+    .indexOf(elementToFind);
+}
